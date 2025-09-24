969767ea38c6e4f9b2a44b81022df4ba75597bbe
luzAnaa <ana.breginski.ifpr.2023@gmail.com>
2025-09-24 16:15:29 -0300

    exercício resolvido
    falta adicionar caso não digite nada

---
+++ js/item3.js
@@ -0,0 +1,14 @@
+var frutas=[]
+var f
+do{
+    f = prompt("Digite a fruta:")
+    if(f!=null && f!=""){
+        frutas.push(f)
+    }
+}while(f!=null)
+
+alert(frutas)
+
+for(var i = 0; i<frutas.length; i++){
+    document.write("<h2>" + frutas[i] + "<h2>")
+}
