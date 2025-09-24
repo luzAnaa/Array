b2f375cafbd6213f32774ab08e00731043dc3262
luzAnaa <ana.breginski.ifpr.2023@gmail.com>
2025-09-24 15:50:29 -0300

    sem exercício

---
+++ js/item1.js
@@ -0,0 +1,17 @@
+var a=[4, 8, 3, 5, 9] /*Criado vetor já preenchido*/
+
+/*Exibindo o array de inteiros*/
+alert(a)
+
+/*Exibindo um elemento do array*/
+alert(a[4])
+
+/*Alterar um valor do array*/
+a[0]=7
+alert(a)
+
+/*Percorrendo o vetor usando laço*/
+for(var i = 0; i<a.length; i++){
+    document.write("<h2>" + a[i] + "</h2>")
+}
+
